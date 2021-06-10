147e7f13f860036b078390c1d2f3d2f13df0f956
Haripriya129 <69579424+Haripriya129@users.noreply.github.com>
2021-06-10 09:38:45 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a=0,b=1;
+    int sum,n;
+    cin>>n;
+    cout<<a<<" "<<b<<" ";
+    while(sum<n)
+    {
+        sum=a+b;
+        a=b;
+        b=sum;
+        cout<<sum<<" ";
+    }
+}
